a8e7a367ae4652ecbe010b09f3e07dddb7848697
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-02 16:04:11 -0500

    feature(searchBar):added

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
@@ -8,7 +9,11 @@ function Header() {
                  <img
                     src="https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg"
                     alt=""
-                  />
+                />
+            <div className="header__input">
+                <SearchIcon />
+                <input type='text' />
+            </div>
             <div className='header__middle'></div>
 
             <div className='header__right'></div>
