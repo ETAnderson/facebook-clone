6e8474ca9439ba4c411dae2106512a7bf35f412c
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-02 16:11:31 -0500

    renamed master to main

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
 
 function Header() {
     return (
@@ -12,9 +16,13 @@ function Header() {
                 />
             <div className="header__input">
                 <SearchIcon />
-                <input type='text' />
+                <input type='text' /> 
             </div>
-            <div className='header__middle'></div>
+            <div className='header__middle'>
+                <div className="header__option">
+                    <HomeIcon fontSize="large" />
+                </div>
+            </div>
 
             <div className='header__right'></div>
         </div>
