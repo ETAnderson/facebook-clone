6d14b18dace5b79fb0c57f48c23e6e5cc2f2cba4
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-02 14:58:47 -0500

    feature(firebase): added src/firebase.js with the firebase provided config for facebook-clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBdRxp8TbsnZFEc9AArTI3zMG9jlIR8qZk",
+    authDomain: "facebook-clone-79a01.firebaseapp.com",
+    projectId: "facebook-clone-79a01",
+    storageBucket: "facebook-clone-79a01.appspot.com",
+    messagingSenderId: "536148626486",
+    appId: "1:536148626486:web:5efbd6892d8c68ba5e1c97",
+    measurementId: "G-K5919TF19B"
+  };
