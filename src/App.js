4632ac3b2ed651b1e8d5564bbf99d65925642972
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-02 17:01:23 -0500

    bug(header): sstyling not applying to component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     // BEM naming convention
     <div className="app">
-     <h1>Lets build a facebook clone!</h1>
 
      <Header />
 
--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,11 @@ import FlagIcon from '@material-ui/icons/Flag';
 import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
 import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
 import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import { Avatar, Icon, IconButton } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function Header() {
     return (
@@ -36,8 +41,25 @@ function Header() {
                     <SupervisedUserCircleIcon fontSize="large" />
                 </div>
             </div>
+            <div className='header__right'>
+                <div className="header__info">
+                    <Avatar />
+                    <h4>etanderson</h4>
+                </div>
 
-            <div className='header__right'></div>
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+                <IconButton>
+                    <ForumIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>
+                <IconButton>
+                    <ExpandMoreIcon />
+                </IconButton>
+            </div>
         </div>
     )
 }
