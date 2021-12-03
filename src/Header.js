062a79e648dba0d1c282a02e4083926655ef1770
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-02 22:50:37 -0500

    feature(header(styling)): added

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ import FlagIcon from '@material-ui/icons/Flag';
 import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
 import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
 import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
-import { Avatar, Icon, IconButton } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import ForumIcon from '@material-ui/icons/Forum';
 import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
@@ -17,14 +17,14 @@ function Header() {
         <div className='header'>
             <div className='header__left'></div>
                  <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg"
+                    src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Facebook_icon.png"
                     alt=""
                 />
             <div className="header__input">
                 <SearchIcon />
                 <input type='text' /> 
             </div>
-            <div className='header__middle'>
+            <div className='header__center'>
                 <div className="header__option">
                     <HomeIcon fontSize="large" />
                 </div>
