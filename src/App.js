f44f49469d008bb9f8d5f1ed911037eb7d5af53c
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-03 00:45:57 -0500

    feature(StoryReel): static StoryReel placeholders added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
-import Feed from './Feed';
+
 
 function App() {
   return (
@@ -10,11 +11,11 @@ function App() {
     <div className="app">
      <Header />
 
-    <div className="app_body"></div>
-    <Sidebar />
-    <Feed />
-    {/* Feed */}
-    {/* Widgets */}
+    <div className="app_body">
+      <Sidebar />
+      <Feed />
+      {/* Widgets */}
+    </div>
     </div>
   );
 }
--- src/StoryReel.js
+++ src/StoryReel.js
@@ -1,14 +1,35 @@
 import React from 'react'
+import Story from './Story'
 import './StoryReel.css'
 
 function StoryReel() {
     return (
         <div className='storyReel'>
-            <Story />
-            <Story />
-            <Story />
-            <Story />
-            <Story />
+            <Story 
+                image='https://avatars.githhttps://www.facebook.com/photo/?fbid=10156942703899665&set=a.496350324664ubusercontent.com/u/2357717?v=4'
+                profileSrc='https://avatars.githubusercontent.com/u/2357717?v=4'
+                title='etanderson'
+            />
+            <Story 
+                image='https://avatars.githhttps://www.facebook.com/photo/?fbid=10156942703899665&set=a.496350324664ubusercontent.com/u/2357717?v=4'
+                profileSrc='https://avatars.githubusercontent.com/u/2357717?v=4'
+                title='etanderson'
+            />
+            <Story 
+                image='https://avatars.githhttps://www.facebook.com/photo/?fbid=10156942703899665&set=a.496350324664ubusercontent.com/u/2357717?v=4'
+                profileSrc='https://avatars.githubusercontent.com/u/2357717?v=4'
+                title='etanderson'
+            />
+            <Story 
+                image='https://avatars.githhttps://www.facebook.com/photo/?fbid=10156942703899665&set=a.496350324664ubusercontent.com/u/2357717?v=4'
+                profileSrc='https://avatars.githubusercontent.com/u/2357717?v=4'
+                title='etanderson'
+            />
+            <Story 
+                image='https://avatars.githhttps://www.facebook.com/photo/?fbid=10156942703899665&set=a.496350324664ubusercontent.com/u/2357717?v=4'
+                profileSrc='https://avatars.githubusercontent.com/u/2357717?v=4'
+                title='etanderson'
+            />
         </div>
     )
 }
