87250417aab45913f35caea52ba182bc33056fce
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-06 14:19:48 -0500

    fix(messageSender text, eslint): corrected messageSender__input placeholder texts spacing, and added line ignore for eslint conventions about invoked but uncalled variables

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -45,7 +45,7 @@ function MessageSender() {
                         value={input}
                         onChange={(e) => setInput(e.target.value)} 
                         className='messageSender__input'
-                        placeholder={`What's on your mind,${user.displayName} ?`} />
+                        placeholder={`What's on your mind, ${user.displayName} ?`} />
                     <input 
                         value={imageUrl}
                         onChange={(e) => setImageUrl(e.target.value)}
